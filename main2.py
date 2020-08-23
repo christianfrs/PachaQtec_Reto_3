a218aa1a358345f25f952bbe337a98245e14a339
christianfrs <christian.frs@gmail.com>
2020-08-22 22:44:33 -0500

    Pregunta del nivel 2 mejorada

--- main2.py
+++ main2.py
@@ -4,6 +4,11 @@ try:
     n_notas = int(input("¿Cuántas notas quieres ingresar por alumno? "))
     alumnos=[]
     notas=[] 
+    may_notas=[]
+    men_notas=[]
+    prom_notas=[]
+    resumen=[]
+    headers=[]
     for x in range(n_alumnos):
         print(f'Ingresa el nombre del alumno {x+1}')
         alumno = input('Nombre: ')
@@ -20,10 +25,18 @@ try:
              else:
                 print ('Nota incorrecta. Ingresa una nota entre 0 y 20')      
         notas.sort()
+        may_notas.append(notas[-1])
+        men_notas.append(notas[0])
+        prom_notas.append(sum(notas)/len(notas))
         print(f'La mayor nota de {alumnos[x]} es: {notas[-1]}')
         print(f'La menor nota de {alumnos[x]} es: {notas[0]}')
         print(f'La nota promedio de {alumnos[x]} es: {sum(notas)/len(notas)}')
         print('***************************')
+        notas.clear()
+    resumen = zip (alumnos,may_notas,men_notas,prom_notas)
+    from tabulate import tabulate
+    print(tabulate(tuple(resumen),headers=['Alumno','Nota max','Nota min','Nota prom']))
+
 except Exception as e:
     print (e)
     print("Debes registrar un número entero")
